c51fabe85345c2b7e521eb5b5abcd15f0714d742
DavideAlovisio13 <alovisiod@gmail.com>
2024-04-17 13:14:47 +0200

    Update MainComponent.vue and store.js with new functionality

---
+++ src/components/MainComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-6 col-lg-3">
+
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+    export default {
+        name: 'HeaderComponent',
+        data() {
+            return {
+                
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/store/store.js
+++ src/store/store.js
@@ -22,6 +22,4 @@ export const store = reactive({
             query: "a",
         }
     },
-    
-
 })
