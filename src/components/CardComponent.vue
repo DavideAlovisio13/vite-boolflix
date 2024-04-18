b445cffe3950e3d6535d9127318ac3259d4e0652
DavideAlovisio13 <alovisiod@gmail.com>
2024-04-18 09:21:45 +0200

    Update CardComponent.vue and MainComponent.vue to display movie posters, language flags, and add input field for name

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -5,7 +5,7 @@
       :src="store.imagesUrl + image"
       alt="Card image cap"
     />
-    <div class="card-body">
+    <div class="card-body d-none">
       <h5 class="card-title">{{ title }}</h5>
       <p class="card-text">{{ overview }}</p>
       <p class="card-text">{{ lang.toUpperCase() }}</p>
@@ -50,37 +50,16 @@ export default {
   border-radius: 0.5rem;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
   transition: transform 0.3s ease-in-out;
+
+  &:hover {
+    transform: scale(1.05);
+  }
+
+  .card-img-top {
+    width: 300px;
+    height: 400px;
+    object-fit: cover;
+  }
 }
 
-.card:hover {
-  transform: scale(1.05);
-}
-
-.card-img-top {
-  width: 100%;
-  height: auto;
-  object-fit: cover;
-  border-top-left-radius: 0.5rem;
-  border-top-right-radius: 0.5rem;
-}
-
-.card-body {
-  padding: 1rem;
-}
-
-.card-title {
-  font-size: 1.25rem;
-  font-weight: bold;
-  margin-bottom: 0.5rem;
-}
-
-.card-text {
-  font-size: 1rem;
-  color: #555;
-}
-
-span {
-    height: 16px;
-    width: 18px;
-}
 </style>
